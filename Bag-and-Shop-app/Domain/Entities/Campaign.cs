128c85bdb293e84ada5554ed77824325753ff433
EduardoLolli <edufernandeslolli@gmail.com>
2026-03-02 20:23:24 -0300

    adicionada entidade e testes com conecxao com o banco

--- Bag-and-Shop-app/Domain/Entities/Campaign.cs
+++ Bag-and-Shop-app/Domain/Entities/Campaign.cs
@@ -2,18 +2,18 @@
 
 namespace Bag_and_Shop_app.Domain.Entities
 {
-    public class Campaign
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public int SystemId { get; set; }
-        public SystemEntity System { get; set; } = null!;
-        public int Players_limit { get; set; } = 10;
-        public int MasterId { get; set; }
-        public User Master { get; set; } = null!;
-        public string Campaign_code { get; set; } = string.Empty;
-        public AttributeEnum Attribute_definition { get; set; }
-        public Boolean Is_store_open { get; set; }
-        public Boolean Enable_multiclasses { get; set; }
-    }
+  public class Campaign
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int SystemId { get; set; }
+    public SystemEntity System { get; set; } = null!;
+    public int Players_limit { get; set; } = 5;
+    public int MasterId { get; set; }
+    public User Master { get; set; } = null!;
+    public string Campaign_code { get; set; } = string.Empty;
+    public AttributeEnum Attribute_definition { get; set; }
+    public Boolean Is_store_open { get; set; }
+    public Boolean Enable_multiclasses { get; set; }
+  }
 }
---
+++ Bag-and-Shop-app/Migrations/20260302231731_teste_migration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bag_and_Shop_app.Migrations
+{
+    /// <inheritdoc />
+    public partial class teste_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SystemEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    System_code = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemEntities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Campaigns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SystemId = table.Column<int>(type: "int", nullable: false),
+                    Players_limit = table.Column<int>(type: "int", nullable: false),
+                    MasterId = table.Column<int>(type: "int", nullable: false),
+                    Campaign_code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Attribute_definition = table.Column<int>(type: "int", nullable: false),
+                    Is_store_open = table.Column<bool>(type: "bit", nullable: false),
+                    Enable_multiclasses = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campaigns", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Campaigns_SystemEntities_SystemId",
+                        column: x => x.SystemId,
+                        principalTable: "SystemEntities",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Campaigns_Users_MasterId",
+                        column: x => x.MasterId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campaigns_MasterId",
+                table: "Campaigns",
+                column: "MasterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Campaigns_SystemId",
+                table: "Campaigns",
+                column: "SystemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Campaigns");
+
+            migrationBuilder.DropTable(
+                name: "SystemEntities");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
